81717562ab6239e715910f930acec7eef4cc8b82
Pavel Smokorovskiy <pavvel@gmail.com>
2020-05-26 21:34:40 +0300

    modified SearchTest

--- src/test/java/com/rbinternational/openapi/marketplace/test/stepdefinitions/SearchTest.java
+++ src/test/java/com/rbinternational/openapi/marketplace/test/stepdefinitions/SearchTest.java
@@ -1,17 +1,36 @@
 package com.rbinternational.openapi.marketplace.test.stepdefinitions;
 
 import com.rbinternational.openapi.marketplace.test.stepdefinitions.pages.SearchPage;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class SearchTest {
 
-    WebDriver driver;
-    SearchPage page;
+    private static WebDriver driver;
+    private static final String URL = "http://localhost:4200/";
+    private SearchPage page;
 
-    @Before
-    public void setUp(){
+    @BeforeClass
+    public static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "../chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get(URL);
+    }
 
+    @Test
+    public void someTest(){
+
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        driver.quit();
     }
 
 
