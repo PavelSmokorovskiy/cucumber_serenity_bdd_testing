7cd21141fdc5ec3c1cd2afdea4727b569372c0cf
Pavel Smokorovskiy <pavvel@gmail.com>
2020-06-17 03:29:25 +0300

    fixed driver timeout

--- src/test/java/com/rbinternational/openapi/marketplace/test/steps/Service.java
+++ src/test/java/com/rbinternational/openapi/marketplace/test/steps/Service.java
@@ -1,12 +1,19 @@
 package com.rbinternational.openapi.marketplace.test.steps;
 
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.util.ArrayList;
+import java.util.Objects;
 
 import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getDriver;
 
 public class Service {
 
     public static void switchToTheSecondTab() {
+        new WebDriverWait(getDriver(), 3) {
+        }.until((ExpectedCondition<Boolean>) driver ->
+                (Objects.requireNonNull(driver).getWindowHandles().size() > 1));
         getDriver().switchTo().window(new ArrayList<>(getDriver().getWindowHandles()).get(1));
     }
 
